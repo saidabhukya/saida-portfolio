797c50de654a414e76e06e6b1b85b6b593f730fd
saidabhukya <saidabhukya060@gmail.com>
2026-08-13 18:46:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -225,39 +225,3 @@ function calculateAgriInvestment() {
 
     document.getElementById('agri-result-box').style.display = 'block';
 }
-const form = document.getElementById('form');
-const submitBtn = form.querySelector('button[type="submit"]');
-
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const formData = new FormData(form);
-    formData.append("access_key", "cf86a1b7-2481-4946-ad8f-bd6e948b7c01");
-
-    const originalText = submitBtn.textContent;
-
-    submitBtn.textContent = "Sending...";
-    submitBtn.disabled = true;
-
-    try {
-        const response = await fetch("https://api.web3forms.com/submit", {
-            method: "POST",
-            body: formData
-        });
-
-        const data = await response.json();
-
-        if (response.ok) {
-            alert("Success! Your message has been sent.");
-            form.reset();
-        } else {
-            alert("Error: " + data.message);
-        }
-
-    } catch (error) {
-        alert("Something went wrong. Please try again.");
-    } finally {
-        submitBtn.textContent = originalText;
-        submitBtn.disabled = false;
-    }
-});
